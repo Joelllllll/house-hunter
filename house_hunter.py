91549e88acf6f915a91d4011ca64b34a0fa5125d
Joel Hendy <joel.hendy@lexer.com.au>
2020-02-01 23:00:27 +1100

    Soo many things

--- house_hunter.py
+++ house_hunter.py
@@ -7,8 +7,12 @@ import queue
 import requests
 import threading
 
-"""This script takes a json schema of house features/properties and searches the website Domain.com
-It currently only prints the URLs to screen but I want to do more"""
+
+import folium
+import webbrowser
+
+"""This script takes a json schema of house features/properties and searches the website Domain.com for properties
+and plots the geopoints on a map useing folium"""
 
 LOG = logging.getLogger("house_hunter")
 logging.basicConfig(level = logging.INFO)
@@ -20,16 +24,25 @@ LISTINGS_ENDPOINT = "https://api.domain.com.au/v1/listings/"
 
 ## Maximum number of pages to paginate through
 MAX_PAGES = 10
+MAP_FILE = "index.html"
 
 class house_hunter_domain:
-    def __init__(self, client_id, client_secret):
+    def __init__(self, client_id, client_secret, properties_fpath):
         self.client_id = client_id
         self.client_secret = client_secret
         self.auth = self.get_auth()
+        LOG.info(f"Successfully retrieved token")
         self.id_queue = queue.Queue()
+        ## Load the house properties json file
+        try:
+            self.house_properties = json.load(open(properties_fpath))
+        except IndexError as e:
+            LOG.info("Make sure to supply a house properties json file as a command line argument")
+            raise e
 
     def get_auth(self):
         "Returns the Auth needed for the requests header"
+        LOG.info("Attempting to obtain access token")
         response = requests.post(TOKEN_URL, data = {"client_id": self.client_id,
             "client_secret": self.client_secret,
             "grant_type":"client_credentials",
@@ -37,46 +50,59 @@ class house_hunter_domain:
             "Content-Type":"text/json"}) 
         try:
             return {"Authorization":"Bearer " f"""{response.json()["access_token"]}"""}
-        except KeyError:
-            LOG.info("There was an error when getting your access token")
-            LOG.info(f"{response.text}")
+        except KeyError as e:
+            LOG.info(f"There was an error when obtaining your access token {respsonse.text}")
+            raise e
         
     def get_listing_ids(self):
         "Puts all the rental ids in a queue for get_listing_info() to consume from"
         for page_number in range(1, MAX_PAGES+1):
-            house_properties["page"] = page_number
-            response = requests.post(RESIDENTIAL_ENDPOINT, headers = self.auth, json = house_properties)
-            for obj in response.json():
-                self.id_queue.put(obj["listing"]["id"])
+            self.house_properties["page"] = page_number
+            response = requests.post(RESIDENTIAL_ENDPOINT, headers = self.auth, json = self.house_properties)
+            try:
+                for obj in response.json():
+                    self.id_queue.put(obj["listing"]["id"])
+            except ValueError as e:
+                LOG.error(f"There was an issue retrieving the listing ids \n {response.headers}")
+                raise e
 
     def get_listing_info(self):
+        lats, lons, urls, prices = [], [], [], []
         "Grabs rental ids from the id_queue and gets the rental info"
         while not self.id_queue.empty():
-            response = requests.get(f"{LISTINGS_ENDPOINT}{self.id_queue.get()}", headers= self.auth)
-            print(response.json()["seoUrl"])
+            data = requests.get(f"{LISTINGS_ENDPOINT}{self.id_queue.get()}", headers= self.auth).json()
+            lat.append(data["geoLocation"]["latitude"])
+            lon.append(data["geoLocation"]["longitude"])
+            price.append(data["priceDetails"]["displayPrice"])
+            url.append(data["seoUrl"])
+        
+        LOG.info("Finished retrieving listings")
+        return lats, lons, urls, prices
 
-def load_house_properties():
-    "Retuns the user provided house properties"
-    try:
-        prop = json.load(open(args.properties_fpath))
-    except IndexError:
-        LOG.warning("Make sure to supply a house properties json file as a command line argument")
-    return prop
+def plot_rentals(lats, lons, urls):
+    "Plots the lat/lons on a map along with displaying domain urls"
+    m = folium.Map([max(lats), max(lons)])
+    LOG.info(f"Adding {len(urls)} set(s) of geo points to map")
+    for url, lat, lon in zip(urls, lats, lons):
+        folium.Marker([lat, lon],
+         popup=url).add_to(m)
+    m.save(MAP_FILE)
+    webbrowser.open("file://" + os.path.realpath(MAP_FILE))
+    LOG.info(f"Cleaning up file {MAP_NAME}")
+    os.remove(MAP_FILE)
 
+
+def run(client_id, client_secret, properties_fpath):
+    test = house_hunter_domain(client_id, client_secret, properties_fpath)
+    LOG.info("Attempting to obtain rental ids")
+    test.get_listing_ids()
+    ## Grab all properties we want to plot
+    lats, lons, urls, prices = test.get_listing_info()
+    plot_rentals(lats, lons, urls)
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description='Takes rental search parameters and returns urls')
-    parser.add_argument('--properties', action='store', help='The search parameters', type=str, dest='properties_fpath', required=True)
+    parser = ArgumentParser(description="Takes rental search parameters and returns plots map")
+    parser.add_argument("--properties", action="store", help="The search parameters", type=str, dest="properties_fpath", required=True)
     args = parser.parse_args()
 
-##TODO: Make this into a run method ?
-    ## Get house properties
-    house_properties = load_house_properties()
-    test = house_hunter_domain(os.getenv("CLIENTID"), os.getenv("CLIENTSECRET"))
-    ## We can get the listing info as soon as we have 1 id so use threading here
-    LOG.info("Getting rental ids")
-    ids = threading.Thread(target=test.get_listing_ids())
-    ids.start()
-    info = threading.Thread(target=test.get_listing_info)
-    info.start()
-    LOG.info("Finished retrieving listings")
+    run(os.getenv("CLIENTID"), os.getenv("CLIENTSECRET"), args.properties_fpath)
